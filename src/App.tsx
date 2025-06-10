6889546b9c87fae67fe804e833751b72b915847c
Mar. Leska <mariales.leska@gmail.com>
2025-06-10 13:18:07 +0300

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Layout from "./components/Layout.tsx";
+import Todo from "./components/Todo.tsx";
+
+function App() {
+ return (
+     <>
+      <Layout>
+        <Todo/>
+      </Layout>
+     </>
+ )
+
+}
+
+export default App
