89e6f4be139ebe0454926a83eef63ab831ad1f14
Mar. Leska <mariales.leska@gmail.com>
2025-06-10 14:05:02 +0300

    feat: add new case and button

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,9 +1,13 @@
 import TodoForm from "./TodoForm.tsx";
 import TodoList from "./TodoList.tsx";
-import {useReducer} from "react";
+import {useEffect, useReducer} from "react";
 //import {useState} from "react";
 import type { TodoProps, Action } from "../types.ts"
 
+const getInitialTodos = () => {
+    const stored =  localStorage.getItem("todos")
+    return stored ? JSON.parse(stored) : []
+}
 const todoReducer =
     (state: TodoProps[], action: Action): TodoProps[] => {
         switch (action.type) {
@@ -26,6 +30,8 @@ const todoReducer =
                                                     ? {...todo, completed:!todo.completed}
                                                     : todo
                 )
+            case "CLEAR_ALL":
+                return []
             default:
                 return state
         }
@@ -33,9 +39,20 @@ const todoReducer =
 
 const Todo = () => {
 
-    const [todos, dispatch] = useReducer(todoReducer, [])
+    const [todos, dispatch] = useReducer(todoReducer, [], getInitialTodos)
     //const [todos, setTodos] = useState([])
     console.log(todos)
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos))
+    }, [todos]);
+
+    const handleClearAll = () => {
+        dispatch({type: "CLEAR_ALL"})
+    }
+
+    const totalTasks: number = todos.length;
+    const completedTasks: number = todos.filter(t => t.completed).length;
+    const activeTasks: number = totalTasks - completedTasks;
 
     return (
         <>
@@ -45,6 +62,21 @@ const Todo = () => {
                 </h1>
                 <TodoForm dispatch = {dispatch}/>
                 <TodoList todos = {todos} dispatch = {dispatch}/>
+                {todos.length > 0 && (
+                    <>
+                        <div className="flex justify-between border-t pt-2 mt-4 text-icon-text">
+                            <span>Total: {totalTasks}</span>
+                            <span>Active: {activeTasks}</span>
+                            <span>Completed: {completedTasks}</span>
+                        </div>
+                        <div className="text-end mt-4">
+                            <button className="bg-list-icon text-white py-2 px-4 rounded cursor-pointer" onClick={handleClearAll}>
+                                Clear All
+                            </button>
+                        </div>
+                    </>
+                )}
+
             </div>
         </>
     )
