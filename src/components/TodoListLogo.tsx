ffc3e5c06cc54ed26045fce7f5d758f559f2adab
Mar. Leska <mariales.leska@gmail.com>
2025-07-15 15:45:24 +0300

    feat: change the src image

--- src/components/TodoListLogo.tsx
+++ src/components/TodoListLogo.tsx
@@ -1,7 +1,7 @@
 const TodoListLogo = () => {
     return (
         <>
-            <img className="h-18 my-4 mx-4" src="/todo-list-logo.jpg" alt="List Logo"/>
+            <img className="h-18 my-4 mx-4" src={`${import.meta.env.BASE_URL}todo-list-logo.jpg`}  alt="List Logo"/>
         </>
     )
 }
